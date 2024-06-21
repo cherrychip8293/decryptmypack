60d5eef40fdde3b65b9f8f66960c9015ec2e6fec
Restart <45609733+RestartFU@users.noreply.github.com>
2024-06-20 20:31:54 -0400

    Create test_server.go

---
+++ testing/test_server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"github.com/df-mc/dragonfly/server"
+	"github.com/df-mc/dragonfly/server/player"
+	"github.com/df-mc/dragonfly/server/player/chat"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	log := logrus.New()
+	log.Formatter = &logrus.TextFormatter{ForceColors: true}
+	log.Level = logrus.InfoLevel
+
+	chat.Global.Subscribe(chat.StdoutSubscriber{})
+
+	cf := server.DefaultConfig()
+	cf.Network.Address = ":19169"
+
+	conf, err := cf.Config(log)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	srv := conf.New()
+	srv.CloseOnProgramEnd()
+
+	srv.Listen()
+
+	for srv.Accept(func(p *player.Player) {
+		fmt.Println(p.Addr().String())
+	}) {
+	}
+}
